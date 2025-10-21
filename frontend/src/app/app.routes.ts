2cfbe22435c16232d365aa09bd6fc95a391aac72
martina verbauwen <verbauwen2003@gmail.com>
2025-10-21 19:50:00 -0300

    cambios en los archivos app.routes.ts app.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,18 +4,6 @@ import { RegisterComponent } from './register.component';
 
 import { UsuariosComponent } from './usuarios.component';
 import { Routes } from '@angular/router';
-import { Routes, CanMatchFn, Router } from '@angular/router';
-import { inject } from '@angular/core';
-
-const authGuard: CanMatchFn = () => {
-  const hasToken = !!localStorage.getItem('token');
-  return hasToken ? true : inject(Router).parseUrl('/login');
-};
-
-const loginGuard: CanMatchFn = () => {
-  const hasToken = !!localStorage.getItem('token');
-  return hasToken ? inject(Router).parseUrl('/agenda') : true;
-};
 
 import { TurnosMedicoComponent } from './turnos-medico.component';
 import { AgendarTurnoComponent } from './agendar-turno.component';
@@ -29,16 +17,4 @@ export const routes: Routes = [
 		{ path: 'turnos-medico', component: TurnosMedicoComponent },
 	{ path: 'agendar-turno', component: AgendarTurnoComponent },
 
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  {
-    path: 'login',
-    canMatch: [loginGuard],
-    loadComponent: () => import('./features/login/login.component').then(m => m.LoginComponent)
-  },
-  {
-    path: 'agenda',
-    canMatch: [authGuard],
-    loadComponent: () => import('./features/agenda-turnos/agenda-turnos.component').then(m => m.AgendaTurnosComponent)
-  },
-  { path: '**', redirectTo: 'login' }
 ];
